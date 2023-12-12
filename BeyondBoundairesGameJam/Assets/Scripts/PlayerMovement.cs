fbc98da4b2068540fa1d5446ddf378efd7642f9f
AJ <24.samantha.jackson@ksd.org>
2023-12-12 09:40:44 -0800

    Fixed Player Anims

--- BeyondBoundairesGameJam/Assets/Scripts/PlayerMovement.cs
+++ BeyondBoundairesGameJam/Assets/Scripts/PlayerMovement.cs
@@ -42,6 +42,7 @@ public class PlayerMovement : MonoBehaviour
     {
         myAnimator.SetFloat("AnimMoveX", moveInput.x);
         myAnimator.SetFloat("AnimMovey", moveInput.y);
+        myAnimator.SetFloat("AnimMoveMagnitude", moveInput.magnitude);
     }
    
 }

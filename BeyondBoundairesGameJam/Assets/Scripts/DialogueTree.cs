b6ec580df488dad72d8f43d96d24fe0a0ea6e3c5
AJ <24.samantha.jackson@ksd.org>
2023-12-13 08:52:36 -0800

    Added stuff for dialogue

--- BeyondBoundairesGameJam/Assets/Scripts/DialogueTree.cs
+++ BeyondBoundairesGameJam/Assets/Scripts/DialogueTree.cs
@@ -1,10 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 [CreateAssetMenu(fileName = "New Chat", menuName = "Dialogue")]
 public class DialogueTree : ScriptableObject
 {
-   [TextArea (3, 6)]
-    [SerializeField] string dialogue = "Type your dialogue here!";
+    [TextArea(3, 6)]
+     public string[] lines;
+
+    [SerializeField] TextMeshProUGUI dialogueText;
+    [SerializeField] TextMeshProUGUI nameText;
+    [SerializeField] GameObject dialoguePanel;
+    public void ShowDialogue(string dialogue, string name)
+    {
+        nameText.text = name + "...";
+        dialogueText.text = dialogue;
+        dialoguePanel.SetActive(true);
+    }
+    public void EndDialogue()
+    {
+        nameText.text = null;
+        dialogueText.text = null; ;
+        dialoguePanel.SetActive(false);
+    }
 }
